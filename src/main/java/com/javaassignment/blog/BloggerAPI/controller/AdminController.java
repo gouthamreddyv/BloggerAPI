7d510e7e602fbcbd68b5e430be8e8fc7e0649e71
gouthamreddyv <gouthamreddyv@gmail.com>
2022-10-21 12:30:43 +1100

    modified the structure

--- src/main/java/com/javaassignment/blog/BloggerAPI/controller/AdminController.java
+++ src/main/java/com/javaassignment/blog/BloggerAPI/controller/AdminController.java
@@ -7,15 +7,10 @@ import com.javaassignment.blog.BloggerAPI.model.BlogPostRes;
 import com.javaassignment.blog.BloggerAPI.service.AdminService;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -34,18 +29,5 @@ public class AdminController {
     public BlogPostRes createPost(@Valid @RequestBody BlogPostReq blogPostReq) throws APIException {
         return adminService.savePost(blogPostReq);
     }
-
-   /* @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
-    }*/
 
 }
